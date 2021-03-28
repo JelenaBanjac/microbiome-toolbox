8255669e4b713544bde1743b7f42dd3a4d913a84
Jelena <msjelenabanjac@gmail.com>
2021-03-29 00:06:11 +0200

    deploy with gunicorn

--- webapp/pages/page3.py
+++ webapp/pages/page3.py
@@ -45,8 +45,42 @@ layout = dhc.Div([
 )
 
 page_content = [
-    # Abundance plot in general
+    
+    dhc.Hr(),
+    dhc.H4("Only Trajectory Line"),
     dhc.Div(id='page-3-display-value-0', children=loading_img),
+    #dcc.Graph(figure=fig1),
+    dhc.Br(),
+    dhc.Hr(),
+    dhc.H4("Longitudinal Subject's Data"),
+    dhc.Div(id='page-3-display-value-1', children=loading_img),
+    #dcc.Graph(figure=fig7),
+    dhc.Br(),
+    dhc.Hr(),
+    dhc.H4("Universality: Linear Difference between Group Trajectories"),
+    dhc.Div(id='page-3-display-value-2', children=loading_img),
+    #dcc.Graph(figure=fig2),
+    dhc.Br(),
+    dhc.Hr(),
+    dhc.H4("Universality: Nonlinear Difference between Group Trajectories"),
+    dhc.Div(id='page-3-display-value-3', children=loading_img),
+    #dcc.Graph(figure=fig3),
+    dhc.Br(),
+    dhc.Hr(),
+    dhc.H4("Reference vs. Non-reference Longitudinal Trajectories"),
+    dhc.Div(id='page-3-display-value-4', children=loading_img),
+    #dcc.Graph(figure=fig4),
+    dhc.Br(),
+    dhc.Hr(),
+    dhc.H4("Differentiation: Linear Reference vs. Non-reference Difference Between Trajectories"),
+    dhc.Div(id='page-3-display-value-5', children=loading_img),
+    #dcc.Graph(figure=fig5),
+    dhc.Br(),
+    dhc.Hr(),
+    dhc.H4("Differentiation: Nonlinear (Spline) Reference vs. Non-reference Difference Between Trajectories"),
+    dhc.Div(id='page-3-display-value-6', children=loading_img),
+    #dcc.Graph(figure=fig6),
+    dhc.Br(),
 ]
 
 @app.callback(
@@ -116,50 +150,287 @@ def display_value(session_id):
 
     fig1,  mae, r2, pi_median = plot_trajectory(estimator=estimator, df=val1, feature_cols=bacteria_names, df_other=None, group=None, nonlinear_difference=True, start_age=0, limit_age=limit_age, plateau_area_start=plateau_area_start, time_unit_size=time_unit_size, time_unit_name=time_unit_name, website=True);
 
+    ret_val = dhc.Div([])
+    if df is not None:
+        ret_val =  [
+            dcc.Graph(figure=fig1),
+                    ]
+
+    return ret_val
+
+
+@app.callback(
+    Output('page-3-display-value-1', 'children'),
+    Input('session-id', 'children'))
+def display_value(session_id):
+    df = read_dataframe(session_id, None)
+    bacteria_names = get_bacteria_names(df, bacteria_fun=lambda x: x.startswith("bacteria_"))
+    
+    if max(df.age_at_collection.values) < 100:
+        plateau_area_start=None #45
+        time_unit_size=1
+        time_unit_name="days"
+        limit_age = 60
+    else:
+        plateau_area_start=None  #700
+        time_unit_size=30
+        time_unit_name="months"
+        limit_age = 750
+
+    estimator = train(df, feature_cols=bacteria_names, Regressor=Regressor, parameters=parameters, param_grid=param_grid, n_splits=2, file_name=None)
+
+    # # healthy unseen data - Test-1
+    # val1 = df[df.classification_dataset_type=="Test-1"]
+    # # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    # other = df[df.classification_dataset_type.isin(["Train-2","Test-2"])]
+    # # unhealthy unseen data - Test2
+    # val2 =  df[df.classification_dataset_type=="Test-2"]
+     # healthy unseen data - Test-1
+    val1 = df[df.dataset_type=="Validation"]
+    # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    other = df[df.dataset_type=="Test"]
+    # unhealthy unseen data - Test2
+    #val2 =  df[df.classification_dataset_type=="Test-2"]
+
     fig7,  mae, r2, pi_median = plot_trajectory(estimator=estimator, df=val1, feature_cols=bacteria_names, df_other=None, group=None, nonlinear_difference=True, start_age=0, limit_age=limit_age, plateau_area_start=plateau_area_start, time_unit_size=time_unit_size, time_unit_name=time_unit_name, website=True, longitudinal_mode="markers+lines");
+
+
+
+    ret_val = dhc.Div([])
+    if df is not None:
+        ret_val =  [
+            dcc.Graph(figure=fig7),
+                    ]
+
+    return ret_val
+
+
+@app.callback(
+    Output('page-3-display-value-2', 'children'),
+    Input('session-id', 'children'))
+def display_value(session_id):
+    df = read_dataframe(session_id, None)
+    bacteria_names = get_bacteria_names(df, bacteria_fun=lambda x: x.startswith("bacteria_"))
+    
+    if max(df.age_at_collection.values) < 100:
+        plateau_area_start=None #45
+        time_unit_size=1
+        time_unit_name="days"
+        limit_age = 60
+    else:
+        plateau_area_start=None  #700
+        time_unit_size=30
+        time_unit_name="months"
+        limit_age = 750
+
+    estimator = train(df, feature_cols=bacteria_names, Regressor=Regressor, parameters=parameters, param_grid=param_grid, n_splits=2, file_name=None)
+
+    # # healthy unseen data - Test-1
+    # val1 = df[df.classification_dataset_type=="Test-1"]
+    # # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    # other = df[df.classification_dataset_type.isin(["Train-2","Test-2"])]
+    # # unhealthy unseen data - Test2
+    # val2 =  df[df.classification_dataset_type=="Test-2"]
+     # healthy unseen data - Test-1
+    val1 = df[df.dataset_type=="Validation"]
+    # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    other = df[df.dataset_type=="Test"]
+    # unhealthy unseen data - Test2
+    #val2 =  df[df.classification_dataset_type=="Test-2"]
 
 
     fig2,  mae, r2, pi_median = plot_trajectory(estimator=estimator, df=val1, feature_cols=bacteria_names, df_other=None, group="group", linear_difference=True, start_age=0, limit_age=limit_age, plateau_area_start=plateau_area_start, time_unit_size=time_unit_size, time_unit_name=time_unit_name, website=True);
 
+
+    ret_val = dhc.Div([])
+    if df is not None:
+        ret_val =  [
+            dcc.Graph(figure=fig2),
+                    ]
+
+    return ret_val
+
+
+
+@app.callback(
+    Output('page-3-display-value-3', 'children'),
+    Input('session-id', 'children'))
+def display_value(session_id):
+    df = read_dataframe(session_id, None)
+    bacteria_names = get_bacteria_names(df, bacteria_fun=lambda x: x.startswith("bacteria_"))
+    
+    if max(df.age_at_collection.values) < 100:
+        plateau_area_start=None #45
+        time_unit_size=1
+        time_unit_name="days"
+        limit_age = 60
+    else:
+        plateau_area_start=None  #700
+        time_unit_size=30
+        time_unit_name="months"
+        limit_age = 750
+
+    estimator = train(df, feature_cols=bacteria_names, Regressor=Regressor, parameters=parameters, param_grid=param_grid, n_splits=2, file_name=None)
+
+    # # healthy unseen data - Test-1
+    # val1 = df[df.classification_dataset_type=="Test-1"]
+    # # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    # other = df[df.classification_dataset_type.isin(["Train-2","Test-2"])]
+    # # unhealthy unseen data - Test2
+    # val2 =  df[df.classification_dataset_type=="Test-2"]
+     # healthy unseen data - Test-1
+    val1 = df[df.dataset_type=="Validation"]
+    # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    other = df[df.dataset_type=="Test"]
+    # unhealthy unseen data - Test2
+    #val2 =  df[df.classification_dataset_type=="Test-2"]
+
     fig3,  mae, r2, pi_median = plot_trajectory(estimator=estimator, df=val1, feature_cols=bacteria_names, df_other=None, group="group", nonlinear_difference=True, start_age=0, limit_age=limit_age, plateau_area_start=plateau_area_start,  time_unit_size=time_unit_size, time_unit_name=time_unit_name, website=True);
+
+
+    ret_val = dhc.Div([])
+    if df is not None:
+        ret_val =  [
+            dcc.Graph(figure=fig3),
+                    ]
+
+    return ret_val
+
+
+
+@app.callback(
+    Output('page-3-display-value-4', 'children'),
+    Input('session-id', 'children'))
+def display_value(session_id):
+    df = read_dataframe(session_id, None)
+    bacteria_names = get_bacteria_names(df, bacteria_fun=lambda x: x.startswith("bacteria_"))
+    
+    if max(df.age_at_collection.values) < 100:
+        plateau_area_start=None #45
+        time_unit_size=1
+        time_unit_name="days"
+        limit_age = 60
+    else:
+        plateau_area_start=None  #700
+        time_unit_size=30
+        time_unit_name="months"
+        limit_age = 750
+
+    estimator = train(df, feature_cols=bacteria_names, Regressor=Regressor, parameters=parameters, param_grid=param_grid, n_splits=2, file_name=None)
+
+    # # healthy unseen data - Test-1
+    # val1 = df[df.classification_dataset_type=="Test-1"]
+    # # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    # other = df[df.classification_dataset_type.isin(["Train-2","Test-2"])]
+    # # unhealthy unseen data - Test2
+    # val2 =  df[df.classification_dataset_type=="Test-2"]
+     # healthy unseen data - Test-1
+    val1 = df[df.dataset_type=="Validation"]
+    # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    other = df[df.dataset_type=="Test"]
+    # unhealthy unseen data - Test2
+    #val2 =  df[df.classification_dataset_type=="Test-2"]
+
 
     fig4,  mae, r2, pi_median = plot_trajectory(estimator=estimator, df=val1, feature_cols=bacteria_names, df_other=other, group=None, nonlinear_difference=True, start_age=0, limit_age=limit_age, plateau_area_start=plateau_area_start, time_unit_size=time_unit_size, time_unit_name=time_unit_name, website=True);
 
+
+    ret_val = dhc.Div([])
+    if df is not None:
+        ret_val =  [
+            dcc.Graph(figure=fig4),
+                    ]
+
+    return ret_val
+
+
+
+@app.callback(
+    Output('page-3-display-value-5', 'children'),
+    Input('session-id', 'children'))
+def display_value(session_id):
+    df = read_dataframe(session_id, None)
+    bacteria_names = get_bacteria_names(df, bacteria_fun=lambda x: x.startswith("bacteria_"))
+    
+    if max(df.age_at_collection.values) < 100:
+        plateau_area_start=None #45
+        time_unit_size=1
+        time_unit_name="days"
+        limit_age = 60
+    else:
+        plateau_area_start=None  #700
+        time_unit_size=30
+        time_unit_name="months"
+        limit_age = 750
+
+    estimator = train(df, feature_cols=bacteria_names, Regressor=Regressor, parameters=parameters, param_grid=param_grid, n_splits=2, file_name=None)
+
+    # # healthy unseen data - Test-1
+    # val1 = df[df.classification_dataset_type=="Test-1"]
+    # # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    # other = df[df.classification_dataset_type.isin(["Train-2","Test-2"])]
+    # # unhealthy unseen data - Test2
+    # val2 =  df[df.classification_dataset_type=="Test-2"]
+     # healthy unseen data - Test-1
+    val1 = df[df.dataset_type=="Validation"]
+    # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    other = df[df.dataset_type=="Test"]
+    # unhealthy unseen data - Test2
+    #val2 =  df[df.classification_dataset_type=="Test-2"]
+
     fig5 = plot_2_trajectories(estimator, val1, other, feature_cols=bacteria_names, degree=2, plateau_area_start=plateau_area_start, limit_age=limit_age, start_age=0, time_unit_size=time_unit_size, time_unit_name=time_unit_name, linear_pval=True, nonlinear_pval=False, img_file_name=None, website=True)
+
+
+    ret_val = dhc.Div([])
+    if df is not None:
+        ret_val =  [
+            dcc.Graph(figure=fig5),
+                    ]
+
+    return ret_val
+
+
+
+@app.callback(
+    Output('page-3-display-value-6', 'children'),
+    Input('session-id', 'children'))
+def display_value(session_id):
+    df = read_dataframe(session_id, None)
+    bacteria_names = get_bacteria_names(df, bacteria_fun=lambda x: x.startswith("bacteria_"))
+    
+    if max(df.age_at_collection.values) < 100:
+        plateau_area_start=None #45
+        time_unit_size=1
+        time_unit_name="days"
+        limit_age = 60
+    else:
+        plateau_area_start=None  #700
+        time_unit_size=30
+        time_unit_name="months"
+        limit_age = 750
+
+    estimator = train(df, feature_cols=bacteria_names, Regressor=Regressor, parameters=parameters, param_grid=param_grid, n_splits=2, file_name=None)
+
+    # # healthy unseen data - Test-1
+    # val1 = df[df.classification_dataset_type=="Test-1"]
+    # # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    # other = df[df.classification_dataset_type.isin(["Train-2","Test-2"])]
+    # # unhealthy unseen data - Test2
+    # val2 =  df[df.classification_dataset_type=="Test-2"]
+     # healthy unseen data - Test-1
+    val1 = df[df.dataset_type=="Validation"]
+    # unhealthy unseen data - Test2 & unhealthy seen data - Train-2
+    other = df[df.dataset_type=="Test"]
+    # unhealthy unseen data - Test2
+    #val2 =  df[df.classification_dataset_type=="Test-2"]
+
 
     fig6 = plot_2_trajectories(estimator, val1, other, feature_cols=bacteria_names, degree=2, plateau_area_start=plateau_area_start, limit_age=limit_age, start_age=0, time_unit_size=time_unit_size, time_unit_name=time_unit_name, linear_pval=False, nonlinear_pval=True, img_file_name=None, website=True)
 
     ret_val = dhc.Div([])
     if df is not None:
-        ret_val =  [dhc.Hr(),
-                    dhc.H4("Only Trajectory Line"),
-                    dcc.Graph(figure=fig1),
-                    dhc.Br(),
-                    dhc.Hr(),
-                    dhc.H4("Longitudinal Subject's Data"),
-                    dcc.Graph(figure=fig7),
-                    dhc.Br(),
-                    dhc.Hr(),
-                    dhc.H4("Universality: Linear Difference between Group Trajectories"),
-                    dcc.Graph(figure=fig2),
-                    dhc.Br(),
-                    dhc.Hr(),
-                    dhc.H4("Universality: Nonlinear Difference between Group Trajectories"),
-                    dcc.Graph(figure=fig3),
-                    dhc.Br(),
-                    dhc.Hr(),
-                    dhc.H4("Reference vs. Non-reference Longitudinal Trajectories"),
-                    dcc.Graph(figure=fig4),
-                    dhc.Br(),
-                    dhc.Hr(),
-                    dhc.H4("Differentiation: Linear Reference vs. Non-reference Difference Between Trajectories"),
-                    dcc.Graph(figure=fig5),
-                    dhc.Br(),
-                    dhc.Hr(),
-                    dhc.H4("Differentiation: Nonlinear (Spline) Reference vs. Non-reference Difference Between Trajectories"),
-                    dcc.Graph(figure=fig6),
-                    dhc.Br(),
+        ret_val =  [
+            dcc.Graph(figure=fig6),
                     ]
 
     return ret_val
-
