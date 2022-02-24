8d7087c08327e543dea107304f9dee030644847a
Jelena <msjelenabanjac@gmail.com>
2022-02-24 20:31:44 +0100

    add dashboard citation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="microbiome-toolbox", 
-    version="1.0.0",
+    version="1.0.2",
     author="Jelena Banjac, Shaillay Kumar Dogra, Norbert Sprenger",
     author_email="msjelenabanjac@gmail.com, ShaillayKumar.Dogra@rd.nestle.com, norbert.sprenger@rdls.nestle.com",
     maintainer="Jelena Banjac",
--- webapp/layout/navbar/navbar.py
+++ webapp/layout/navbar/navbar.py
@@ -3,14 +3,27 @@ from dash import dcc
 from dash import html as dhc
 from utils.constants import home_location
 
-navbar = dbc.Navbar(
-    dbc.Container(
-        dcc.Link(
-            dbc.NavbarBrand("Microbiome Toolbox", className="ml-2"),
-            href=home_location,
-        )
-    ),
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink([
+                "Known issues",
+                dhc.I(
+                    title="Known issues",
+                    className="fa fa-bug",
+                    style={"marginLeft": "10px"},
+                ),
+        ], href="https://github.com/JelenaBanjac/microbiome-toolbox/issues", target="_blank",)),
+        dbc.NavItem(dbc.NavLink([
+                "Cite paper",
+                dhc.I(
+                    title="Cite paper",
+                    className="fa fa-quote-left",
+                    style={"marginLeft": "10px"},
+                ),
+        ], href="https://www.biorxiv.org/content/10.1101/2022.02.14.479826v1", target="_blank",)),
+    ],
+    brand="Microbiome Toolbox",
+    brand_href=home_location,
     color="dark",
     dark=True,
-    className="mb-5",
 )
